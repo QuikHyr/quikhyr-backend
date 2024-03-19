784768a69c628f28f400007c281c561092f6b6ca
Noah <noahjoh11@gmail.com>
2024-03-19 20:26:24 +0530

    Updated firebase configuration

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,24 @@
 import * as admin from "firebase-admin";
 
-const serviceAccount = require("../serviceAccountKey.json");
+require("dotenv").config();
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: "googleapis.com",
+};
 
 // Initialize the Firebase Admin SDK
 const adminApp = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
 
 console.log("[database]: connected to firestore");
